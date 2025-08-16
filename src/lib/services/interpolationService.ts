f67a73764ed4af92a08dc525dbda862a600ade1e
rawatanuj07 <rawatanuj07@gmail.com>
2025-08-16 14:04:43 +0530

    removed files

--- src/lib/services/interpolationService.ts
+++
@@ -1,9 +0,0 @@
-export function interpolate(ts: number, before: any, after: any): number {
-  const tsBefore = before.timestamp;
-  const priceBefore = before.price;
-  const tsAfter = after.timestamp;
-  const priceAfter = after.price;
-
-  const ratio = (ts - tsBefore) / (tsAfter - tsBefore);
-  return priceBefore + (priceAfter - priceBefore) * ratio;
-}
